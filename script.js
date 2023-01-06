18492f6adb6eccd70795c3bc23a8d7d8703735fd
BiancaDias <bianca.dias@ufv.br>
2023-01-06 10:42:44 -0300

    botão finalizar pedido pronto

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ function selecionaPrato(botaoClicado){
     if(botaoClicadoAnteriormenteP!==null){
         botaoClicadoAnteriormenteP.classList.remove('selecionadoComida');
     }
-
+    
     const elemento = document.querySelector(botaoClicado);
     elemento.classList.add('selecionadoComida');
+
+    const botaoClicadoAnteriormenteB = document.querySelector('.selecionadoBebida');
+    const botaoClicadoAnteriormenteS = document.querySelector('.selecionadoSobremesa');
+    const comida = document.querySelector('.selecionadoComida');
+    if(botaoClicadoAnteriormenteS!==null && botaoClicadoAnteriormenteB!==null && comida!==null)
+    {
+        fecharPedido();
+    }
 }
 
 function selecionaBebida(botaoClicado){
@@ -18,10 +26,20 @@ function selecionaBebida(botaoClicado){
 
     const elemento = document.querySelector(botaoClicado);
     elemento.classList.add('selecionadoBebida');
+
+    const botaoClicadoAnteriormenteS = document.querySelector('.selecionadoSobremesa');
+    const botaoClicadoAnteriormenteP = document.querySelector('.selecionadoComida');
+    const bebida = document.querySelector('.selecionadoBebida');
+
+    if(botaoClicadoAnteriormenteS!==null && bebida!==null && botaoClicadoAnteriormenteP!==null)
+    {
+        fecharPedido();
+    }
 }
 
 function selecionaSobremesa(botaoClicado){
     const botaoClicadoAnteriormenteS = document.querySelector('.selecionadoSobremesa');
+    
 
     if(botaoClicadoAnteriormenteS!==null){
         botaoClicadoAnteriormenteS.classList.remove('selecionadoSobremesa');
@@ -29,11 +47,35 @@ function selecionaSobremesa(botaoClicado){
 
     const elemento = document.querySelector(botaoClicado);
     elemento.classList.add('selecionadoSobremesa');
-}
 
+    const botaoClicadoAnteriormenteB = document.querySelector('.selecionadoBebida');
+    const botaoClicadoAnteriormenteP = document.querySelector('.selecionadoComida');
+    const sobremesa = document.querySelector('.selecionadoSobremesa');
+    if(botaoClicadoAnteriormenteP!==null && botaoClicadoAnteriormenteB!==null && sobremesa!==null)
+    {
+        fecharPedido();
+    }
+}
+//função chamada quando todos os elementos forem selecionados
 function fecharPedido(){
+    const botaoClicadoAnteriormenteP = document.querySelector('.selecionadoComida');
+    const botaoClicadoAnteriormenteB = document.querySelector('.selecionadoBebida');
+    const botaoClicadoAnteriormenteS = document.querySelector('.selecionadoSobremesa');
     if(botaoClicadoAnteriormenteP!==null && botaoClicadoAnteriormenteB!==null && botaoClicadoAnteriormenteS!==null)
     {
-        console.log("ta indo");
+        const botao = document.querySelector('button');
+        botao.classList.add('liberarPedido');
+        botao.innerHTML= "Fechar pedido";
+    }
+}
+//funcao a ser usada para fechar o pedido e chamar o wpp
+function fecharPedidoDefinitivo(botao){
+    const botaoClicadoAnteriormenteP = document.querySelector('.selecionadoComida');
+    const botaoClicadoAnteriormenteB = document.querySelector('.selecionadoBebida');
+    const botaoClicadoAnteriormenteS = document.querySelector('.selecionadoSobremesa');
+    if(botaoClicadoAnteriormenteP!==null && botaoClicadoAnteriormenteB!==null && botaoClicadoAnteriormenteS!==null)
+    {
+        alert("Bora fechar");
+        
     }
 }
